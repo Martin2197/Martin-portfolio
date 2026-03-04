3c9a041d3ebf3efbe2e8149aa6741e34212c51dc
Martin2197 <martin.villalba2197@gmail.com>
2026-03-04 12:01:07 -0300

    main

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,14 @@
 navLinks.classList.toggle("active");
+
+async function loadProjects() {
+  try {
+    const response = await fetch("http://localhost:3000/projects");
+    const projects = await response.json();
+
+    console.log(projects);
+  } catch (error) {
+    console.error("Error cargando proyectos:", error);
+  }
+}
+
+loadProjects();
